4345fd33226640ad1b9185ef8b73547093a24872
fechnas-git <73794179+fechnas-git@users.noreply.github.com>
2020-11-03 13:11:38 +0100

    Update IsometricPlayerMovement.cs
    Fixed the Collision of Tiles and Objects using 2 Things
    1) LayerMask - I used LayerMasks for Collidable Tiles and Collidable Objects, so the coherent Method only checks what it should, because posToCheck is different for tiles and objects. Also added a seperate method because we need to give half the value of direction. We can probably optimize
    
    2) Tilemap Collider - How this will work in the end really depends on how the collision tiles are drawn, but it successfully detects colliders on the edge of a tile. So you can move on a tile from one side, but not off it, or not on it, from the other side.

--- Assets/Scripts/IsometricPlayerMovement.cs
+++ Assets/Scripts/IsometricPlayerMovement.cs
@@ -9,6 +9,11 @@ public class IsometricPlayerMovement : MonoBehaviour
 
     private bool m_isMoving;
 
+    [SerializeField]
+    private LayerMask collidableObjects;
+    [SerializeField]
+    private LayerMask collidableTiles;
+
     void Awake()
     {
         m_Rbody = GetComponent<Rigidbody2D>();
@@ -21,22 +26,26 @@ public class IsometricPlayerMovement : MonoBehaviour
 
     private void Movement()
     {
-        if (Input.GetKeyDown(KeyCode.W) && !m_isMoving && CheckCollider((transform.position) + IsoGame.Access.Directions.up))
+        if (Input.GetKeyDown(KeyCode.W) && !m_isMoving && CheckCollidableObject((transform.position) + IsoGame.Access.Directions.up)
+            && CheckCollidableTile((transform.position) + IsoGame.Access.Directions.up / 2.0f))
         {
-            StartCoroutine(MovePlayer(IsoGame.Access.Directions.up)); //Use 1/2 Cell Size here depending on the move direction, maybe theres a inherit grid solution
+            StartCoroutine(MovePlayer(IsoGame.Access.Directions.up));
         }
 
-        if (Input.GetKeyDown(KeyCode.A) && !m_isMoving && CheckCollider((transform.position) + IsoGame.Access.Directions.left))
+        if (Input.GetKeyDown(KeyCode.A) && !m_isMoving && CheckCollidableObject((transform.position) + IsoGame.Access.Directions.left)
+            && CheckCollidableTile((transform.position) + IsoGame.Access.Directions.left / 2.0f))
         {
             StartCoroutine(MovePlayer(IsoGame.Access.Directions.left));
         }
 
-        if (Input.GetKeyDown(KeyCode.S) && !m_isMoving && CheckCollider((transform.position) + IsoGame.Access.Directions.down))
+        if (Input.GetKeyDown(KeyCode.S) && !m_isMoving && CheckCollidableObject((transform.position) + IsoGame.Access.Directions.down)
+             && CheckCollidableTile((transform.position) + IsoGame.Access.Directions.down / 2.0f))
         {
             StartCoroutine(MovePlayer(IsoGame.Access.Directions.down));
         }
 
-        if (Input.GetKeyDown(KeyCode.D) && !m_isMoving && CheckCollider((transform.position) + IsoGame.Access.Directions.right))
+        if (Input.GetKeyDown(KeyCode.D) && !m_isMoving && CheckCollidableObject((transform.position) + IsoGame.Access.Directions.right)
+             && CheckCollidableTile((transform.position) + IsoGame.Access.Directions.right / 2.0f))
         {
             StartCoroutine(MovePlayer(IsoGame.Access.Directions.right));
         }
@@ -53,27 +62,35 @@ public class IsometricPlayerMovement : MonoBehaviour
 
         while (elapsedTime < 0.2f) //this 0.2f might be interchangable
         {
-            transform.position = Vector3.Lerp(originalPosition, targetPosition, (elapsedTime / 0.2f) *IsoGame.Access.LerpSpeed);
+            transform.position = Vector3.Lerp(originalPosition, targetPosition, (elapsedTime / 0.2f) * IsoGame.Access.LerpSpeed);
 
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         transform.position = targetPosition;
-        
+
         m_isMoving = false;
     }
 
-    private bool CheckCollider(Vector3 posToCheck)
+    private bool CheckCollidableObject(Vector3 posToCheck)
     {
 
-        Collider2D col = Physics2D.OverlapPoint(posToCheck);
-        if (col != null)
+        if (Physics2D.OverlapPoint(posToCheck, collidableObjects) != null)
         {
-            Debug.Log("Found him " + col.name);
+            Debug.Log("Found him " + Physics2D.OverlapPoint(posToCheck, collidableObjects).name);
             return false;
         }
+        return true;
+    }
+    private bool CheckCollidableTile(Vector3 posToCheck)
+    {
 
+        if (Physics2D.OverlapPoint(posToCheck, collidableTiles) != null)
+        {
+            Debug.Log("Found him " + Physics2D.OverlapPoint(posToCheck, collidableTiles).name);
+            return false;
+        }
         return true;
     }
 }
